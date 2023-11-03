2feee903f9c68fd04f96d7aa4559562289b39c8c
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-11-03 09:40:21 +0100

    Initial commit

---
+++ src/main/java/com/example/data22aiproject/Data22AiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.data22aiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data22AiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data22AiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data22aiproject/Data22AiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data22aiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data22AiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
